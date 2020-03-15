6b1f5aac26884adddd7bcffb04875daf8e159496
nick5616 <30281766+nick5616@users.noreply.github.com>
2020-03-15 14:39:44 -0500

    made some functionality work. still a little buggy.

--- project/build/action.js
+++ project/build/action.js
@@ -2,52 +2,71 @@ class task {
     desc;
     start = "0000";
     end = "2399";
-    task(d, s, e){
-        desc = d;
-        start = s;
-        end = e;
-    }
-    task(d){
-        desc = d;
+    constructor(d, s, e){
+        console.log("invoked main constructor");
+        this.desc = d;
+        this.start = s;
+        this.end = e;
     }
 }
 class routine{
-    board = [];
+    data = [];
     appendTask(task){
-        this.board.push(task);
+        this.data.push(task);
     }
     removeLastTask(task){
-        this.board.pop();
+        this.data.pop();
     }
 }
 
 var board = new routine();
 function addTaskToDisplay(task){
-    var htmlString = '<div class = "example-task">'+
-    '<input class = "custom-tf description" value = "Breakfast"></input>'+
+    var htmlString = '<div class = "task">'+
+    '<input class = "custom-tf description" value = "'+task.desc+'"></input>'+
     'from'+
-    '<input class = "custom-tf start" value = "9:00 am"></input>'+
+    '<input class = "custom-tf start" value = "'+task.start+'"></input>'+
     'to'+
-    '<input class = "custom-tf end" value = "10:00 am"></input>'+
-    '<button class = "custom-btn"> ENTER </button>'+
+    '<input class = "custom-tf end" value = "'+task.end+'"></input>'+
     '</div>';
     //htmlString.append();
     $(".routine").append(htmlString);
 }
-$(document).on('keyup',function(e) {
+
+function hideUploadInfo(){
+    $(".how-to-upload").hide();
+}
+
+function showUploadInfo(){
+    $(".how-to-upload").show();
+}
+
+$(document).on('keyup', function(e) {
     if(e.which == 8){
         //backspace
         console.log("backspace");
+        //remove task
+        $(".routine").children().last().remove();
+        console.log("removed task. this is the new length", board.data.length);
+        if(board.data.length < 1){
+            showUploadInfo();
+        }
     }
     if(e.which == 13) {
         //enter
+        
         var d = $(".description").val();
         var s = $(".start").val();
         var e = $(".end").val();
         console.log("fetched vals", d, s, e);
         var myBoy = new task(d, s, e);
+        console.log("created task", myBoy);
         board.appendTask(myBoy);
         addTaskToDisplay(myBoy);
+        console.log("board.length", board.data.length);
+        if(board.data.length > 0) {
+            console.log("gonna hide it")
+            hideUploadInfo();
+        }
         console.log('state of routine', board);
     }
 });
